0570950046db079be99805cd0369748e5c708557
Timo Reichl <thetredev@gmail.com>
2023-09-17 17:29:46 +0200

    pkg/target_definition: Use full-length words for parameters
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- pkg/target_definition.go
+++ pkg/target_definition.go
@@ -40,17 +40,19 @@ func (this *TargetDefinition) findField(fieldName string) *structs.Field {
 	return nil
 }
 
-func (this *TargetDefinition) FieldStringValue(fieldName string, defContext *DefinitionContext) (string, error) {
+func (this *TargetDefinition) FieldStringValue(fieldName string, definitionContext *DefinitionContext) (string, error) {
 	field := this.findField(fieldName)
 
 	if field == nil {
 		return "", fmt.Errorf("Could not find field `%s`", fieldName)
 	}
 
-	return fmt.Sprintf("%s/%s", defContext.DefinitionPath, field.Value().(string)), nil
+	return fmt.Sprintf("%s/%s", definitionContext.DefinitionPath, field.Value().(string)), nil
 }
 
-func (this *TargetDefinition) FieldStringArrayValue(fieldName string, defContext *DefinitionContext) ([]string, error) {
+func (this *TargetDefinition) FieldStringArrayValue(
+	fieldName string, definitionContext *DefinitionContext,
+) ([]string, error) {
 	field := this.findField(fieldName)
 
 	if field == nil {
@@ -60,17 +62,17 @@ func (this *TargetDefinition) FieldStringArrayValue(fieldName string, defContext
 	result := []string{}
 
 	for _, value := range field.Value().([]string) {
-		result = append(result, fmt.Sprintf("%s/%s", defContext.DefinitionPath, value))
+		result = append(result, fmt.Sprintf("%s/%s", definitionContext.DefinitionPath, value))
 	}
 
 	return result, nil
 }
 
-func (this *TargetDefinition) DependencyGraph(index int, targetDefs *[]TargetDefinition) []int {
+func (this *TargetDefinition) DependencyGraph(index int, targetDefinitions *[]TargetDefinition) []int {
 	dependencyGraph := []int{index}
 
 	if len(this.Dependencies) > 0 {
-		for i, otherTarget := range *targetDefs {
+		for i, otherTarget := range *targetDefinitions {
 			if slices.Contains(this.Dependencies, otherTarget.Name) {
 				dependencyGraph = append(dependencyGraph, i)
 			}

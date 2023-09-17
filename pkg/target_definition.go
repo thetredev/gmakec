b7e21a9cd77cea68c7ce682c8b6f9f4b29aeb6b0
Timo Reichl <thetredev@gmail.com>
2023-09-17 06:08:05 +0200

    Simplify finding a struct field via new method TargetDefinition.FindField()
    Signed-off-by: Timo Reichl <thetredev@gmail.com>

--- pkg/target_definition.go
+++ pkg/target_definition.go
@@ -26,38 +26,44 @@ func (targetDef *TargetDefinition) ExecuteHooks(step string, workingDir string) 
 	}
 }
 
-func (targetDef *TargetDefinition) FieldStringValue(fieldName string, defContext *DefinitionContext) (string, error) {
+func (targetDef *TargetDefinition) FindField(fieldName string) *structs.Field {
 	fields := structs.Fields(targetDef)
 
 	for _, field := range fields {
 		tag := field.Tag("yaml")
 
 		if tag == fieldName {
-			return fmt.Sprintf("%s/%s", defContext.DefinitionPath, field.Value().(string)), nil
+			return field
 		}
 	}
 
-	return "", fmt.Errorf("Could not find field `%s`", fieldName)
+	return nil
+}
+
+func (targetDef *TargetDefinition) FieldStringValue(fieldName string, defContext *DefinitionContext) (string, error) {
+	field := targetDef.FindField(fieldName)
+
+	if field == nil {
+		return "", fmt.Errorf("Could not find field `%s`", fieldName)
+	}
+
+	return fmt.Sprintf("%s/%s", defContext.DefinitionPath, field.Value().(string)), nil
 }
 
 func (targetDef *TargetDefinition) FieldStringArrayValue(fieldName string, defContext *DefinitionContext) ([]string, error) {
-	fields := structs.Fields(targetDef)
+	field := targetDef.FindField(fieldName)
 
-	for _, field := range fields {
-		tag := field.Tag("yaml")
-
-		if tag == fieldName {
-			result := []string{}
-
-			for _, value := range field.Value().([]string) {
-				result = append(result, fmt.Sprintf("%s/%s", defContext.DefinitionPath, value))
-			}
-
-			return result, nil
-		}
+	if field == nil {
+		return nil, fmt.Errorf("Could not find field `%s`", fieldName)
 	}
 
-	return nil, fmt.Errorf("Could not find field `%s`", fieldName)
+	result := []string{}
+
+	for _, value := range field.Value().([]string) {
+		result = append(result, fmt.Sprintf("%s/%s", defContext.DefinitionPath, value))
+	}
+
+	return result, nil
 }
 
 func (targetDef *TargetDefinition) DependencyGraph(index int, targetDefs *[]TargetDefinition) []int {
